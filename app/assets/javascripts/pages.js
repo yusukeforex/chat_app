0ce13b8ea000e163164ac7b88bc572070e08d40a
yusukeforex <momijina@icloud.com>
2019-12-03 19:25:09 +0900

    create chat scaffold

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,8 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function () {
+  var messages = $('#chatbox');
+  function init() {
+    messages.scrollTop(messages[0].scrollHeight);
+  }
+
+  window.setTimeout(init, 50);
+})
